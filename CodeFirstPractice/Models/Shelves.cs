1f218e786953b3e36b8113b57be6179747177222
Jia <jiavon@ualberta.ca>
2020-09-17 15:24:44 -0600

    Completed mockup

--- CodeFirstPractice/Models/Shelves.cs
+++ CodeFirstPractice/Models/Shelves.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace CodeFirstPractice.Models
 {
-    class Shelves
+    [Table("Shelves")] //A table called “Shelves” with:
+    public partial class Shelves
     {
+        //An int primary key called ID.
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id", TypeName = "int(10)")]
+        public int ID { get; set; }
+
+        //A varchar of length 50 called Name.
+        [Column("name", TypeName = "varchar(50)")]
+        [Required]
+        public string Name { get; set; }
+
     }
 }
--- CodeFirstPractice/Models/ShelvesContext.cs
+++ CodeFirstPractice/Models/ShelvesContext.cs
@@ -5,7 +5,47 @@ using System.Text;
 
 namespace CodeFirstPractice.Models
 {
-    class ShelvesContext : DbContext
+    public partial class ShelvesContext : DbContext
     {
+        public ShelvesContext()
+        {
+
+        }
+
+        public ShelvesContext(DbContextOptions<ShelvesContext> options) : base (options)
+        {
+
+        }
+        public DbSet<Shelves> MultiShelves { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            // If we aren't already configured.
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Initialize the connection to a MySQL server.
+                // Use Entity Framework to create a database called “code_first_practice” using your models.
+                optionsBuilder.UseMySql("server=localhost;port=3306;user=root;database=code_first_practice", x => x.ServerVersion("10.4.14-MariaDB")); 
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //will add model creation here
+            modelBuilder.Entity<Shelves>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_general_ci");
+            });
+
+            //Call the partial method in case we add some stuff into another file later
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
