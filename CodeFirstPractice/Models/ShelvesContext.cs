dd3380d09e460ee15f9c9e804b9ede1cc2c645c6
Jia <jiavon@ualberta.ca>
2020-09-17 14:48:18 -0600

    Installed Pomelo and entity framework library, created models folder, created shelvecontext and shelves as table

---
+++ CodeFirstPractice/Models/ShelvesContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeFirstPractice.Models
+{
+    class ShelvesContext : DbContext
+    {
+    }
+}
---
+++ CodeFirstPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeFirstPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
